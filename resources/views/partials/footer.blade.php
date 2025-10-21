5a27b2761b1d29ae8e2d6e4a4cfeeb25825c254f
hazeeqnajmuddin <hazeeqhaeqal@gmail.com>
2025-10-21 19:23:33 +0800

    footer update

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,3 @@
-<footer class="bg-gray-800 text-white text-center py-4 mt-auto">
+<footer class="bg-gray-800 text-white text-center py-5 mt-auto">
     <p>&copy; {{ date('Y') }} AutoMate. All rights reserved.</p>
 </footer>
